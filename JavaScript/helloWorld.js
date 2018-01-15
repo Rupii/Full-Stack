e0787a9366642cd58e8761b5d6515ff14bf97c21
Rupesh <30984969+Rupii@users.noreply.github.com>
2018-01-15 23:08:20 +0530

    Update helloWorld.js

--- JavaScript/helloWorld.js
+++ JavaScript/helloWorld.js
@@ -1,2 +1,2 @@
 console.log("Hello world")
-<!-- to view the console area go to inspect-developer-console of your browser-->
+//to view the console area go to inspect-developer-console of your browser
