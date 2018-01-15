0a32ee7ad1c2d05e05f97f0440af0d9dafaae762
Rupesh <30984969+Rupii@users.noreply.github.com>
2018-01-15 23:23:33 +0530

    Create Operators_data_types.js

---
+++ JavaScript/Operators_data_types.js
@@ -0,0 +1,38 @@
+// Data Type
+alert("Hello!")
+// this just throws a quick greeting
+
+// you can do lot more with JavaScript string
+// there is indexing
+var name = "Rupesh"
+var lastName = "Mudavath"
+// JavaScript uses camel case
+
+console.log(name[4]);
+//gives fourth index starting from zero
+console.log("Strings can vbe cancatinated as "+"this")
+// Undefined
+// any thing that does not have a value or not defined are Undefined
+var temp;
+var see = NaN;
+console.log("all the above are "+typeof(temp));
+
+var temp = false
+// it also has type Boolean
+
+
+// Operators
+
+true == true
+// comparison
+"2" == 2
+// gives true but string not equals to number
+// so we want
+"2" === 2
+// return false
+
+// to get power of a number we use
+4 ** 3
+
+// except this all the other operators are same as of any other programming language
+console.log("you are cool!");
