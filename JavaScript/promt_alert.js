0c4935f8cf8f748f2844d84f00cfaa2001ef19ed
Rupesh <30984969+Rupii@users.noreply.github.com>
2018-01-15 23:30:50 +0530

    Create promt_alert.js

---
+++ JavaScript/promt_alert.js
@@ -0,0 +1,8 @@
+lbs = prompt("enter the weight in lbs? ")
+kgs = lbs * 0.45
+alert("your age is "+kgs+" kgs")
+
+
+// bank deposit
+amount = promt("enter the amount you want to deposit")
+alert(" you have deposited "+amount+"\n thanks for chosing us \n please do visit again"5878)
