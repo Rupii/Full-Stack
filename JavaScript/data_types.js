95966045de5a1650f2f46b73d4077e0988cb0ed0
Rupesh <30984969+Rupii@users.noreply.github.com>
2018-01-15 22:30:36 +0530

    Create data_types.js

---
+++ JavaScript/data_types.js
@@ -0,0 +1,13 @@
+  // JavaScript is a dynamically tpyed language
+  // what means is JavaScript choose the type according to the data
+var name = "Rupesh"
+var lastName = "Mudavath"
+var age = 30
+console.log(name+" "+lastName)
+console.log("Age of "+name+" is "+age)
+
+console.log("Data type of name is "+typeof(name)+" \n Data type of age is "+typeof(age))
+
+// here you can see the data types of both name and age are different
+// But we mentioned both as var
+// This is chosen by JavaScript{ dynamically typed}
